d711873ccac99ae372779dd8ed8a2230c3bd2283
Leon Holub <l_holub@utb.cz>
2023-03-24 17:05:36 +0100

    design upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="cz">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="./styles/styles-front.css" rel="stylesheet">
+</head>
+<body>
+    <main id="main">
+        <nav class="navbar navbar-expand-lg d-flex flex-column position-fixed top-0 w-100 pt-0 z-index-nav main-nav">
+            <div class="container-fluid main-nav--top">
+                <div class="container py-1">
+
+                </div>
+            </div>
+
+            <div class="container main-nav--bottom pt-1">
+                <a class="navbar-brand">
+                    <img src="./images/logo-auto-drabek.png" alt="logo autobazaru">
+                </a>
+
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav d-lg-flex justify-content-around w-100">
+                        <a class="nav-link" aria-current="page" href="index.html">Nabídka</a>
+                        <a class="nav-link" aria-current="page" href="index.html"><?php echo "nlkljsdlkfja";?></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </main>
+
+
+<script src="./scripts/front.bundle.js"></script>
+</body>
+</html>
